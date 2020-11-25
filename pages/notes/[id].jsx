56817a7d8b6ccf926d4d2a33eb6a80d27ec74f7f
Margo <margo.bozhenova@gmail.com>
2020-11-25 23:32:51 +0300

    Add Page component

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Page = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>Note {id}</div>;
+};
+
+export default Page;
